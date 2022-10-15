61912311b9ce03080e09d1f6e23af113d8d04650
Thom <thomayor@gmail.com>
2022-10-15 10:47:25 +0200

    update d'hier soir

--- travelbook/src/App.js
+++ travelbook/src/App.js
@@ -3,27 +3,14 @@ import PageConnect from "./Components/Pages/PageConnection/PageConnect";
 //import PageErreur from "./Components/Elements/pageErreur";
 //import Profil from "./Components/Pages/PageMain/profil/Profil";
 import './App.css';
-<<<<<<< HEAD
-//import PageMain from "./Components/Pages/PageMain/PageMain";
-//import EditProfil from "./Components/Pages/PageMain/profil/EditProfil";
-=======
-import PageMain from "./Components/Pages/PageMain/PageMain";
-import EditProfil from "./Components/Pages/PageMain/profil/EditProfil";
-import PostsTest from "./Components/Elements/PostsTest";
-import { useState } from "react";
->>>>>>> Natalia
+
 const router = createBrowserRouter([
 
 
   {
     path: "/",
-<<<<<<< HEAD
     element: <PageConnect />,
     /* error: <PageErreur /> */
-=======
-    element: <PageConnect/>,
-    error: <PageErreur />
->>>>>>> Natalia
   },
 
 /*   {
@@ -51,27 +38,10 @@ function App() {
 
 
   return (
-<<<<<<< HEAD
     
  <RouterProvider router={router} />
 
  
-=======
-    <div className="App">
- <RouterProvider router={router}/>
-    
-   
-
-  
-  
-    
-    
-    
-    </div>
-
-
-
->>>>>>> Natalia
 
   );
 }
