2ba900de7f7f52ae7451727d6f502f0b13f0c3aa
Vladimir Sinkevitch <sinkevitch06@gmail.com>
2022-10-17 16:50:07 +0200

    lundi fin de cours

---
+++ travelbook/src/Components/Pages/ElementsPage/Footer.js
@@ -0,0 +1,5 @@
+import "./Footer.css";
+function FooterMain() {
+	return <div className="Footer">Footer</div>;
+}
+export default FooterMain;
---
+++ travelbook/src/Components/Pages/ElementsPage/Menu.js
@@ -0,0 +1,13 @@
+import "./Menu.css";
+function MenuMain() {
+	return (
+		<div className="Menus">
+			<div className="deuxButton">
+				<h1>Menu</h1>
+				<button>Accueil</button>
+				<button>Profil</button>
+			</div>
+		</div>
+	);
+}
+export default MenuMain;
