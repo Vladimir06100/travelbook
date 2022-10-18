df1e2d1b2f834a183674dcb762f8658b7fb32ccf
Vladimir Sinkevitch <sinkevitch06@gmail.com>
2022-10-18 18:50:52 +0200

    1

--- travelbook/src/Components/Pages/PageMain/PageMain.js
+++ travelbook/src/Components/Pages/PageMain/PageMain.js
@@ -1,41 +1,39 @@
-import {Link} from 'react-router-dom'; 
-import PostsTest from '../../Elements/PostsTest';
+import { Link } from "react-router-dom";
+import PostsTest from "../../Elements/PostsTest";
 
-function PageMain (){
+function PageMain() {
+	const listeArticles = [
+		{
+			title: "Article1",
+			details:
+				"Un paragraphe est une section de texte en prose vouée au développement",
+		},
+		{
+			title: "Article2",
+			details:
+				"Un paragraphe est une section de texte en prose vouée au développement",
+		},
+		{
+			title: "Article3",
+			details:
+				"Un paragraphe est une section de texte en prose vouée au développement",
+		},
+	];
 
-    const listeArticles= [{'title':'Article1','details':'Un paragraphe est une section de texte en prose vouée au développement'},{'title':'Article2','details':'Un paragraphe est une section de texte en prose vouée au développement',},{'title':'Article3','details':'Un paragraphe est une section de texte en prose vouée au développement',}]
+	return (
+		<div>
+			<nav>
+				<Link to="/pagemain">Accueil</Link>
+				<Link to="/profil">Mon profil</Link>
+			</nav>
 
-    return(
-        <div>
-        <nav>
+			<h2>Page principal</h2>
 
-<Link to="/pagemain">Accueil</Link>
-<Link to="/profil">Mon profil</Link>
-
-        </nav>
-
-        <h2>Page principal</h2>
-
-
-        {
-    listeArticles.map(article => {
-
-        return(<PostsTest title={article.title} details={article.details} />)
-    }
-
-
-        )
-
-    }
-
-
-        </div>
-
-    )
-
-
+			{listeArticles.map((article) => {
+				return <PostsTest title={article.title} details={article.details} />;
+			})}
+		</div>
+	);
 }
-
-
 
 export default PageMain;
